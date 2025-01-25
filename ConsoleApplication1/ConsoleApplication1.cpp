a14ba86e79d4e474b61054bd69cd5be16905f0ea
Студент 9/23. РПО <rpo923@step.lan>
2025-01-25 13:17:40 +0700

    first

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct person
+{
+    int age;
+    string lastname;
+
+};
+
+int main(struct person)
+{
+
+    const int size = 50;
+    struct person people[size] = {{53, "Fadeev"}, {49, "Stanislavchik"}, {52, "Krilov"}};
+    struct person(age);
+
+
+    for (int i = 0; i < size; i++)
+    {
+        if (people[i].lastname.size() >= 2 && people[i].lastname.substr(people[i].lastname.size() - 2) == "ov" && people[i].age >= 50)
+        {
+            cout << "Age: " << people[i].age << " Lastname: " << people[i].lastname << " ";
+        }
+    }
+
+}
+
+
